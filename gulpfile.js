c6d93f6da2d54a5ea7c5f19bc6c87494bbff05ab
Pridecoo <pridecoo@gmail.com>
2020-07-06 22:39:18 +0300

    project start with global and var scss

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ let gulp = require("gulp"),
 
 
 gulp.task("sass", function(){
-  return gulp.src("app/scss/styles.scss")
+  return gulp.src("app/scss/**/*.scss")
             .pipe(sass({outputStyle: "compressed"}))
             .pipe(rename({suffix : ".min"}))
             .pipe(autoprefixer({
